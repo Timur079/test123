3c9898bc7aeac5edd33ed9644da803b002c36ce5
mirzoeffzah <mirzoeff.za@yandex.ru>
2024-01-23 13:09:31 +0300

    1

--- src/123.kt
+++ src/123.kt
@@ -1,20 +1,19 @@
-fun main() {
-    while (true) {
-        val a = readLine()!!.toInt()
-        val b = readLine()!!.toInt()
-        val x = readLine()!!
-        kolkulate(a, b, x)
-        println(kolkulate(a, b, x))
-    println("Нажмите 1 чтобы выйти")
-    val z = readLine()!!
-    if (z == "1") {
-        print("Пока")
-        break
+fun main(){
+    max(readLine()!!)
+    task1()
+    val list1: List<Int> = listOf(1, 2, 3)
+    val list2: MutableList<String> = mutableListOf("1", "2", "3")
+    list2.add("4")
+}
+
+fun max(x: String){
+    val list: List<Int> = x.split(" ").map{
+        it.toInt()
     }
-    else{
-        continue
-        }
-    }}
+    val m = list.maxOf { it }
+    print(m)
+
+}
 
 fun kolkulate(num1:Int, num2:Int, sign:String = "+"):String{
     return when (sign){
@@ -30,4 +29,24 @@ fun kolkulate(num1:Int, num2:Int, sign:String = "+"):String{
 
 
     }
+}
+
+fun task1(){
+    while (true) {
+        val a = readLine()!!.toInt()
+        val b = readLine()!!.toInt()
+        val x = readLine()!!
+        kolkulate(a, b, x)
+        println(kolkulate(a, b, x))
+        println("Нажмите 1 чтобы выйти")
+        val z = readLine()!!
+        if (z == "1") {
+            print("Пока")
+            break
+        }
+        else{
+            continue
+        }
+    }
+
 }
